afc40eccab0f43c8bf80d0f70e31811820e25486
Doug Hirlinger <doughirlinger@gmail.com>
2024-12-19 09:08:43 -0500

    initial security config github login code added

--- src/main/java/org/taskntech/tech_flow/config/SecurityConfig.java
+++ src/main/java/org/taskntech/tech_flow/config/SecurityConfig.java
@@ -1,9 +1,7 @@
 package org.taskntech.tech_flow.config;
 
-import jakarta.servlet.DispatcherType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,9 +9,7 @@ import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
-
-//this annotation exposes SecurityFilterChain bean & enables Spring Security integration with Spring MVC.
-@EnableWebSecurity(debug = true)
+@EnableWebSecurity(debug = true) //this annotation exposes SecurityFilterChain bean.
 public class SecurityConfig {
 
     @Bean
@@ -24,19 +20,13 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
         return http
-                .csrf(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults())
-                .authorizeHttpRequests(auth -> {
-                    auth.dispatcherTypeMatchers(DispatcherType.FORWARD, DispatcherType.ERROR).permitAll();
-                    auth.requestMatchers("/", "/login", "/error").permitAll();
+                .authorizeHttpRequests( auth -> {
+                    //auth.dispatcherTypeMatchers(DispatcherType.FORWARD, DispatcherType.ERROR).permitAll();
+                    auth.requestMatchers("/").permitAll(); //(mvc.pattern("/my/controller/**")).hasAuthority("controller");
                     auth.anyRequest().authenticated();
                 })
-                .oauth2Login(form -> form
-                        .loginPage("/")
-                        .defaultSuccessUrl("/tickets/create", true))
-                .logout(logout -> logout
-                        .logoutSuccessUrl("/")
-                )
+                .oauth2Login(form -> form.defaultSuccessUrl("/", true))
+                //.formLogin(withDefaults()) //form -> form.defaultSuccessUrl("/", true)
                 .build();
     }
 
