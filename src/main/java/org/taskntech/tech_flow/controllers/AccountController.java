772e5743fa4d6791297eab7a457633e9e08369f5
katherinehanra <Kchanra6@gmail.com>
2025-01-01 22:08:19 -0600

    Created @AccountController - In Progress

--- src/main/java/org/taskntech/tech_flow/controllers/AccountController.java
+++ src/main/java/org/taskntech/tech_flow/controllers/AccountController.java
@@ -1,6 +1,5 @@
 package org.taskntech.tech_flow.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
@@ -8,69 +7,37 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-import org.taskntech.tech_flow.data.UserRepository;
-import org.taskntech.tech_flow.models.User;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
+import java.util.Map;
 
 @Controller
 public class AccountController {
 
-    @Autowired
-    private UserRepository userRepository;
-
     @GetMapping("/manage-account")
-    public String showManageAccountPage() {
-        // Changed attributes for "name" and "profilePicturePath" to be handled globally - See GlobalControllerAccountSettings
+    public String showManageAccountPage(Model model, @AuthenticationPrincipal OAuth2User principal) {
+        if (principal != null) {
+            String name = principal.getAttribute("name");
+            String email = principal.getAttribute("email");
+            String username = principal.getAttribute("login");
+
+            if (email == null) email = "Set to private";
+            if (username == null) username = email;
+
+            Map<String, Object> allAttributes = principal.getAttributes();
+            model.addAttribute("name", name);
+            model.addAttribute("email", email);
+            model.addAttribute("username", username);
+            model.addAttribute("allAttributes", allAttributes);
+        }
         return "manage-account";
     }
 
     @PostMapping("/manage-account/update")
     public String updateDisplayName(@RequestParam("displayName") String displayName,
                                     @AuthenticationPrincipal OAuth2User principal) {
-        String email = principal.getAttribute("email");
-        User user = userRepository.findByEmail(email);
+        // Save the updated display name to MySQL for use on account management page.
+        System.out.println("Updated display name for user, " + principal.getAttribute("email") + ", to " + displayName + ".");
 
-        if (user != null) {
-            user.setDisplayName(displayName);
-            userRepository.save(user);
-        }
         return "redirect:/manage-account?success=true";
     }
-
-    private static final String UPLOAD_DIRECTORY = System.getProperty("user.dir") + "/uploads/";
-
-    @PostMapping("/manage-account/upload-profile-picture")
-    public String uploadProfilePicture(@RequestParam("profilePicture") MultipartFile file,
-                                       @AuthenticationPrincipal OAuth2User principal) {
-        if (file.isEmpty()) {
-            return "redirect:/manage-account?error=NoFileUploaded";
-        }
-
-        try {
-            String email = principal.getAttribute("email");
-            User user = userRepository.findByEmail(email);
-
-            if (user != null) {
-                String fileName = "profile-" + user.getUserId() + ".png";
-                Path uploadPath = Paths.get(UPLOAD_DIRECTORY, fileName);
-
-                Files.createDirectories(uploadPath.getParent());
-                Files.write(uploadPath, file.getBytes());
-                user.setProfilePicturePath("/uploads/" + fileName);
-                userRepository.save(user);
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            return "redirect:/manage-account?error=FileUploadFailed";
-        }
-        return "redirect:/manage-account?profilePictureUploaded=true";
-    }
-
 }
