244f369649e880f51fa9a9b5ee8e22d3260e1f9c
katherinehanra <Kchanra6@gmail.com>
2025-01-16 20:17:12 -0600

    change update to main

--- src/main/java/org/taskntech/tech_flow/config/SecurityConfig.java
+++ src/main/java/org/taskntech/tech_flow/config/SecurityConfig.java
@@ -1,9 +1,9 @@
-//Imported from Doug's OAuth feature branch to run his portion of the app - not original code
 package org.taskntech.tech_flow.config;
 
 import jakarta.servlet.DispatcherType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,7 +11,9 @@ import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
-@EnableWebSecurity(debug = true) //this annotation exposes SecurityFilterChain bean.
+
+//this annotation exposes SecurityFilterChain bean & enables Spring Security integration with Spring MVC.
+@EnableWebSecurity(debug = true)
 public class SecurityConfig {
 
     @Bean
@@ -22,13 +24,19 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
         return http
+                .csrf(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> {
                     auth.dispatcherTypeMatchers(DispatcherType.FORWARD, DispatcherType.ERROR).permitAll();
                     auth.requestMatchers("/", "/login", "/error").permitAll();
                     auth.anyRequest().authenticated();
                 })
-                .oauth2Login(form -> form.defaultSuccessUrl("/", true))
-                .logout(logout -> logout.logoutSuccessUrl("/"))
+                .oauth2Login(form -> form
+                        .loginPage("/")
+                        .defaultSuccessUrl("/tickets/create", true))
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/")
+                )
                 .build();
     }
 
